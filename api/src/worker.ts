8f696c68d310f200d94494ded64e75161d3f76cc
Tristan Lavocat <107067429+Tritounet1@users.noreply.github.com>
2026-02-08 20:42:59 +0100

    feat: ajout de l'username et password dans la connexion à redis pour le worker*

--- api/src/worker.ts
+++ api/src/worker.ts
@@ -16,6 +16,8 @@ const REDIS_PORT = parseInt(process.env.REDIS_PORT || "6379");
 const connection = new IORedis({
   host: REDIS_HOST,
   port: REDIS_PORT,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
   maxRetriesPerRequest: null,
 });
 
