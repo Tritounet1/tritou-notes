14380403dc74f5b65029fb0d75f3ab3a94c0496b
Tristan Lavocat <107067429+Tritounet1@users.noreply.github.com>
2026-02-08 20:02:59 +0100

    feat: ajout de l'username et password dans la connexion à redis.

--- api/src/config/queue.ts
+++ api/src/config/queue.ts
@@ -1,12 +1,15 @@
 import { Queue } from "bullmq";
 import IORedis from "ioredis";
 
+// TODO: Rajouter les process.env dans config.
 const REDIS_HOST = process.env.REDIS_HOST || "127.0.0.1";
 const REDIS_PORT = parseInt(process.env.REDIS_PORT || "6379");
 
 const connection = new IORedis({
   host: REDIS_HOST,
   port: REDIS_PORT,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
   maxRetriesPerRequest: null,
 });
 
